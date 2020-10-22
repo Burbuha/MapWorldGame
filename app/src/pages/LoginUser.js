1fea135078219bd416db00bc353e91bd7c0e2932
Burbuha <burbovskaja@gmail.com>
2020-10-22 23:44:57 +0300

    app

---
+++ app/src/pages/LoginUser.js
@@ -0,0 +1,14 @@
+const LoginUser = {
+  id: "login",
+  title: "Карта Мира - Войти",
+  render: (className = "container", ...rest) => {
+    return `
+      <section class="${className}">
+        <h2>Войти</h2>
+        <p>.......</p>
+      </section>
+    `;
+  },
+};
+
+export default LoginUser;
---
+++ app/src/pages/RatingUser.js
@@ -0,0 +1,14 @@
+const RatingUser = {
+  id: "rating",
+  title: "Карта Мира - Рейтинг победителей",
+  render: (className = "container", ...rest) => {
+    return `
+      <section class="${className}">
+        <h2>Рейтинг</h2>
+        <p>.......</p>
+      </section>
+    `;
+  },
+};
+
+export default RatingUser;
