7f04df626ac77af0bebbe138bb87f690335b5f21
Mila <63538228+Burbuha@users.noreply.github.com>
2020-10-22 17:29:54 +0300

    Add files via upload

---
+++ app/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = {
+  render: (customClass = "") => {
+    return `<footer class="footer ${customClass}">
+      <p>&copy; Бурбовская Людмила, 2020 / Все права защищены</p>
+    </footer>`;
+  },
+};
+
+export default Footer;
---
+++ app/src/index.js
@@ -0,0 +1,244 @@
+import HomePage from "./pages/HomePage.js";
+import RulesGame from "./pages/RulesGame.js";
+import RatingUser from "./pages/RatingUser.js";
+import LoginUser from "./pages/LoginUser.js";
+import RegistrationUser from "./pages/RegistrationUser.js";
+import ErrorPage from "./pages/ErrorPage.js";
+
+import Header from "./components/Header.js";
+import Navbar from "./components/NavBar.js";
+import Content from "./components/Content.js";
+import Footer from "./components/Footer.js";
+
+// List of components (from components.js)
+const components = {
+  header: Header,
+  navbar: NavBar,
+  content: Content,
+  footer: Footer,
+};
+// List of supported routes (from pages.js)
+const routes = {
+  main: HomePage,
+  rules: RulesGame,
+  rating: RatingUser,
+  login: LoginUser,
+  registr: RegistrationUser,
+  error: ErrorPage,
+};
+
+/* ----- spa init module --- */
+const mySPA = (function () {
+  /* ------- begin view -------- */
+  function ModuleView() {
+    let myModuleContainer = null;
+    let menu = null;
+    let contentContainer = null;
+    let routesObj = null;
+    let canvas = null;
+    let context = null;
+    let path = null;
+
+    this.init = function (container, routes) {
+      myModuleContainer = container;
+      routesObj = routes;
+      menu = myModuleContainer.querySelector("#mainmenu");
+      contentContainer = myModuleContainer.querySelector("#content");
+    };
+
+    this.renderContent = function (hashPageName) {
+      let routeName = "default";
+
+      if (hashPageName.length > 0) {
+        routeName = hashPageName in routes ? hashPageName : "error";
+      }
+
+      window.document.title = routesObj[routeName].title;
+      contentContainer.innerHTML = routesObj[routeName].render(
+        `${routeName}-page`
+      );
+      this.updateButtons(routesObj[routeName].id);
+    };
+
+    this.updateButtons = function (currentPage) {
+      const menuLinks = menu.querySelectorAll(".mainmenu_link");
+
+      for (
+        let i = 0, menuLinksCount = menuLinks.length;
+        i < menuLinksCount;
+        i++
+      ) {
+        if (currentPage === menuLinks[i].getAttribute("href").slice(1)) {
+          menuLinks[i].classList.add("active");
+        } else {
+          menuLinks[i].classList.remove("active");
+        }
+      }
+    };
+
+    this.drawCanvasMap = function (map, projection) {
+      canvas = d3
+        .select("#map")
+        .append("canvas")
+        .attr("width", map.w)
+        .attr("height", map.h);
+      console.log(canvas);
+
+      context = canvas.node().getContext("2d");
+
+      path = d3.geo.path().projection(projection).context(context);
+    };
+
+    this.drawMap = function (map, drawMap) {
+      context.clearRect(0, 0, map.w, map.h);
+
+      drawMap.projection
+        .rotate([drawMap.rotateAngle, drawMap.rotateX, drawMap.rotateY]) //вращаем глобус
+        .clipAngle(90);
+
+      context.beginPath();
+      path(map.sphere);
+      context.lineWidth = 3;
+      context.strokeStyle = "#000";
+      context.stroke();
+      context.fillStyle = "#668cc2";
+      context.fill();
+
+      drawMap.projection.clipAngle(180); //отрисовываем обратную сторону глобуса
+
+      context.beginPath();
+      path(drawMap.land);
+      context.fillStyle = "#134E5E";
+      context.fill();
+
+      context.beginPath();
+      path(drawMap.grid);
+      context.lineWidth = 0.5;
+      context.strokeStyle = "#00248f";
+      context.stroke();
+
+      drawMap.projection.clipAngle(90); //отрисовываем переднюю сторону глобуса
+
+      context.beginPath();
+      path(drawMap.land);
+      context.fillStyle = "#a98b6f";
+      context.fill();
+      context.lineWidth = 0.5;
+      context.strokeStyle = "#000";
+      context.stroke();
+    };
+  }
+  /* -------- end view --------- */
+  /* ------- begin model ------- */
+  function ModuleModel() {
+    let myModuleView = null;
+
+    this.init = function (view) {
+      myModuleView = view;
+    };
+
+    this.updateState = function () {
+      const hashPageName = window.location.hash.slice(1).toLowerCase();
+      myModuleView.renderContent(hashPageName);
+      if (hashPageName === "main") {
+        this.drawMap();
+      }
+    };
+
+    this.drawMap = function () {
+      const map = {
+        w: 500,
+        h: 500,
+        speed: 0.01,
+        start: Date.now(),
+        sphere: { type: "Sphere" },
+      };
+      //настраиваем проекцию
+      const projection = d3.geo
+        .orthographic() //ортографическая проекция
+        .scale(map.w / 2.5) //коэффициент масштабирования
+        .translate([map.w / 2, map.h / 2]); //центр карты
+      //получаем координатную сетку
+      const graticule = d3.geo.graticule();
+
+      myModuleView.drawCanvasMap(map, projection);
+
+      d3.json("data/world-110m.json", function (error, topo) {
+        if (error) throw error;
+
+        //конвертируем TopoJSON в GeoJSON
+        const land = topojson.feature(topo, topo.objects.land), //контуры материков
+          grid = graticule(); //координатная сетка
+
+        //запускаем requestAnimationFrame
+        d3.timer(() => {
+          const drawMap = {
+            land: land,
+            grid: grid,
+            projection: projection,
+            rotateAngle: map.speed * (Date.now() - map.start),
+            rotateX: -15,
+            rotateY: -15,
+          };
+          myModuleView.drawMap(map, drawMap);
+        });
+      });
+    };
+  }
+
+  /* -------- end model -------- */
+  /* ----- begin controller ---- */
+  function ModuleController() {
+    let myModuleContainer = null;
+    let myModuleModel = null;
+
+    this.init = function (container, model) {
+      myModuleContainer = container;
+      myModuleModel = model;
+
+      // вешаем слушателей на событие hashchange и кликам по пунктам меню
+      window.addEventListener("hashchange", this.updateState);
+
+      this.updateState(); //первая отрисовка
+    };
+
+    this.updateState = function () {
+      myModuleModel.updateState();
+    };
+  }
+  /* ------ end controller ----- */
+
+  return {
+    init: function ({ container, routes, components }) {
+      this.renderComponents(container, components);
+
+      const view = new ModuleView();
+      const model = new ModuleModel();
+      const controller = new ModuleController();
+
+      //связываем части модуля
+      view.init(document.getElementById(container), routes);
+      model.init(view);
+      controller.init(document.getElementById(container), model);
+    },
+
+    renderComponents: function (container, components) {
+      const root = document.getElementById(container);
+      const componentsList = Object.keys(components);
+      for (let item of componentsList) {
+        root.innerHTML += components[item].render("component");
+      }
+    },
+  };
+})();
+/* ------ end app module ----- */
+
+/*** --- init module --- ***/
+document.addEventListener(
+  "DOMContentLoaded",
+  mySPA.init({
+    container: "spa",
+    routes: routes,
+    components: components,
+  })
+);
