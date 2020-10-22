6ea6918378610adb1be554663cd6d385de9f028f
Burbuha <burbovskaja@gmail.com>
2020-10-22 22:33:48 +0300

    remove folder doc

--- doc/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = {
-  render: (customClass = "") => {
-    return `<footer class="footer ${customClass}">
-      <p>&copy; Бурбовская Людмила, 2020 / Все права защищены</p>
-    </footer>`;
-  },
-};
-
-export default Footer;
